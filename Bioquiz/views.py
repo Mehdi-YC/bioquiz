fb578a25377f5700a7fa6c93981e78e6b178fccb
Mehdi YC <44881216+SAVE-POlNT@users.noreply.github.com>
2021-11-14 23:34:10 +0100

    randomizing quiz + adding some comments

--- Bioquiz/views.py
+++ Bioquiz/views.py
@@ -7,54 +7,70 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import authenticate, login as l, logout
 
+import random
 import re
 
-
+# since the categories are not stored in the database , 
+# we need to get the distinct values from the category column in the question model
 def get_categories(): 
     categories = Question.objects.values('category').distinct()
     categories = list(categories) if categories else []
     categories = [cat['category'] for cat in categories]
     return categories
 
+#this function allows us to get the score of a user by it's username
 def get_user_score(name):
     user = User.objects.values('id').filter(username=name)[0]
     score = UserDetails.objects.values('score').filter(user_id=1)[0]
     return dict(score)['score']
 
 
-
+#this questin auto generates questions for the 2 question models : 
+#1 - get a category 
+#2 - get all the possible answers for this question
+#3 - select a random answer
+#4 - select n images from the database that have the same value of this catgory
+#5 - serve this generated question
+#5 - a category has 5 questions
 def list_Question(request):
     q_id=int(request.GET['q_id'])-1
     questions = Question.objects.values().filter(category=request.GET['category'])
-    answers = Answer.objects.values().filter(question_id=2)
     question_answers=[]
     for question in questions:
         answers = Answer.objects.values().filter(question_id=question['id'])
-        images = Image.objects.values().filter(id__in=[int(img) for img in question['id_images'].split(',')])
-        question_answers.append({'question':question,'answers':answers,'images':[image['image_file'] for image in images]})
+        r_answer = random.choice(answers)
+        images = Image.objects.raw("select *  from Bioquiz_image where "+question['category']+"='"+r_answer['answer']+"'")
 
-    if q_id < len(question_answers):
-        return render(request,'questions/question.html',{'qa':question_answers[int(q_id)],'length':len(question_answers),'q_id':q_id+1})
+        images = random.sample(list(images), question['n_image'])
+        question_answers.append({'question':question,'r_answer':r_answer,'answers':answers,'images':[image.image_file for image in images]})
+
+    if q_id <= 5:
+        return render(request,'questions/question.html',{'qa':question_answers[0],'length':5,'q_id':q_id+1})
     return redirect('home')
 
 
 def check_answer(request):
+    category= request.GET['category']
     question_id = request.GET['question']
-    answer_id = int(request.GET['answer'])
+    img =request.GET['img']
+    user_answer = request.GET['answer']
+    img_answer = Image.objects.values().get(image_file=img)
     points = Question.objects.values('point').get(id=question_id)
-    correct_answer_id = Question.objects.values('Correct_answer').get(id=question_id)
-    correct_anwer = Answer.objects.values().get(id=correct_answer_id['Correct_answer'])
     u = UserDetails.objects.get(user_id=request.user.id)
 
-    print(correct_answer_id,answer_id)
-    if answer_id == correct_answer_id['Correct_answer']:
+    # get the correct answer
+    correct_answer = Answer.objects.values().filter(answer=img_answer[category])[0]
+
+    # adding / removing points from the user
+    if img_answer[category] == user_answer:
         print(request.user.id)
         u.score = u.score + int(points['point'])
         u.save()
-        return JsonResponse({'val':True,'answer':dict(correct_anwer)})
+        return JsonResponse({'val':True,'answer':dict(correct_answer)})
+
     u.score = u.score - int(points['point'])
     u.save()
-    return JsonResponse({'val':False,'answer':dict(correct_anwer)})
+    return JsonResponse({'val':False,'answer':dict(correct_answer)})
 
 
 
